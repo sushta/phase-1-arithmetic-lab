31e8b8a94e182d1fd227d9bcfd83829f2b99b0e9
sustapakhreen <sushta48@gmail.com>
2023-03-16 23:28:25 -0700

    Completed Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = (num1 * num2);
+console.log(multiply);
+
+//generates a random number greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+//using mod operators
+let num3 = 22;
+let num4 = 6;
+let mod = (num3 % num4);
+
+let max= Math.max(12, 5, 10, 20, 8);
+console.log(max);
